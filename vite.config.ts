f94575f76c37a6bda913a88b3a9706e5075b1a3f
Jonas Neumann <jonas.a.neumann@accenture.com>
2025-06-02 16:52:36 +0800

    fixes vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  base: 'https://jnn5.github.io/graphql-bag-client/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
