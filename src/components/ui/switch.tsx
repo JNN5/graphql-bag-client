3b45a6dd0807e5f081115da979f0978dac3c4966
Jonas Neumann <jonas.a.neumann@accenture.com>
2025-08-12 17:00:40 +0800

    improves toggle

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -9,19 +9,31 @@ const Switch = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      'peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input',
+      'SwitchRoot peer w-[42px] h-[25px] bg-[var(--black-a9)] rounded-full relative focus:shadow-[0_0_0_2px_black] bg-black',
+      'after:content-[""] after:block after:absolute after:w-[21px] after:h-[21px] after:bg-white after:rounded-full after:top-[1px] after:left-[2px]',
+      'after:transition-transform after:duration-100 after:will-change-transform data-[state=checked]:after:translate-x-[17px]',
       className
     )}
     {...props}
     ref={ref}
-  >
-    <SwitchPrimitives.Thumb
-      className={cn(
-        'pointer-events-none block h-4 w-4 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0'
-      )}
-    />
-  </SwitchPrimitives.Root>
+  />
 ));
 Switch.displayName = SwitchPrimitives.Root.displayName;
 
-export { Switch };
+// Label component as provided in your CSS
+const Label = React.forwardRef<
+  HTMLLabelElement,
+  React.HTMLAttributes<HTMLLabelElement>
+>(({ className, ...props }, ref) => (
+  <label
+    className={cn(
+      'Label text-white text-[15px] leading-none select-none',
+      className
+    )}
+    {...props}
+    ref={ref}
+  />
+));
+Label.displayName = 'Label';
+
+export { Switch, Label };
